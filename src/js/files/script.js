49ac2f5e544ef93890a99ecce7c63fc71334f99f
Ivan Sytnychenko <sytnychenkoivan@gmail.com>
2026-03-21 12:47:12 +0200

    fix bugs

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,14 @@
 // import { isMobile } from "./functions.js";
 
 
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		}
+	});
+});
+
+document.querySelectorAll('.box,.box-toright,.box-toleft,.box-todown').forEach(box => observer.observe(box));
+
+// document.querySelectorAll('.__container').forEach(box => observer.observe(box));
